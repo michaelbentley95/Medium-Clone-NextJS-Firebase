b62e0c8c1b204ffba9c0fdefeddb6ea4d78c7870
michaelbentley95 <44556637+michaelbentley95@users.noreply.github.com>
2022-09-10 17:43:50 -0500

    Implemented Pulling Articles from Firebase

--- medium-cp/context/MediumContext.js
+++ medium-cp/context/MediumContext.js
@@ -11,10 +11,46 @@ const MediumProvider = ({ children }) => {
         const getUsers = async () => {
             const querySnapshot = await getDocs(collection(db, "users"));
 
-            querySnapshot.docs.map((doc) => console.log(doc.data()));
+            setUsers(
+                querySnapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        data: {
+                            ...doc.data(),
+                        },
+                    };
+                })
+            );
         };
 
         getUsers();
+    }, []);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const querySnapshot = await getDocs(collection(db, "articles"));
+
+            setPosts(
+                querySnapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        data: {
+                            body: doc.data().body,
+                            brief: doc.data().brief,
+                            category: doc.data().category,
+                            postLength: doc.data().postLength,
+                            bannerImage: doc.data().bannerImage,
+                            title: doc.data().title,
+                            //comments: doc.data().comments,
+                            postedOn: doc.data().postedOn.toDate(),
+                            author: doc.data().author,
+                        },
+                    };
+                })
+            );
+        };
+
+        getPosts();
     }, []);
 
     return <MediumContext.Provider value={{ posts, users }}>{children}</MediumContext.Provider>;
