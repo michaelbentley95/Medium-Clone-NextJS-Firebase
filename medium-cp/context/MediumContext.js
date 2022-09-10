0b857617271f23d250373c5be590944d62f424de
michaelbentley95 <44556637+michaelbentley95@users.noreply.github.com>
2022-09-10 17:18:17 -0500

    Set Up Connection and Retrieve User Data

---
+++ medium-cp/context/MediumContext.js
@@ -0,0 +1,23 @@
+import { collection, getDocs, setDoc, doc } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+
+const MediumContext = createContext();
+const MediumProvider = ({ children }) => {
+    const [users, setUsers] = useState([]);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const querySnapshot = await getDocs(collection(db, "users"));
+
+            querySnapshot.docs.map((doc) => console.log(doc.data()));
+        };
+
+        getUsers();
+    }, []);
+
+    return <MediumContext.Provider value={{ posts, users }}>{children}</MediumContext.Provider>;
+};
+
+export { MediumContext, MediumProvider };
--- medium-cp/pages/index.js
+++ medium-cp/pages/index.js
@@ -1,15 +1,19 @@
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import PostCard from "../components/PostCard";
+import { useContext } from "react";
+import { MediumContext } from "../context/MediumContext";
+
+const styles = {
+    wrapper: "bg-white text-black mx-auto",
+    main: "flex justify-center",
+    container: "max-w-7xl flex-1",
+    postsList: "flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3",
+};
 
 export default function Home() {
-    const styles = {
-        wrapper: "bg-white text-black mx-auto",
-        main: "flex justify-center",
-        container: "max-w-7xl flex-1",
-        postsList:
-            "flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3",
-    };
+    const { posts } = useContext(MediumContext);
+    console.log(posts);
     return (
         <div className={styles.wrapper}>
             <Header />
